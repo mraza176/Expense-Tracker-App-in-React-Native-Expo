3eb85c65deb1450951c616b1884103b1194a7c3e
mraza176 <mrazaraheel12@gmail.com>
2025-03-21 20:54:04 +0500

    Added statistics tab

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,15 +1,192 @@
-import { StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
+import { BarChart } from "react-native-gifted-charts";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { scale, verticalScale } from "@/utils/styling";
+import { useAuth } from "@/contexts/authContext";
+import {
+  fetchMonthlyStats,
+  fetchWeeklyStats,
+  fetchYearlyStats,
+} from "@/services/transactionService";
+import SegmentedControl from "@react-native-segmented-control/segmented-control";
 import ScreenWrapper from "@/components/ScreenWrapper";
-import Typo from "@/components/Typo";
+import Header from "@/components/Header";
+import Loading from "@/components/Loading";
+import TransactionList from "@/components/TransactionList";
 
 const StatisticsScreen = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [chartData, setChartData] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  const [chartLoading, setChartLoading] = useState(false);
+
+  const { user } = useAuth();
+
+  const getWeeklyStats = async () => {
+    setChartLoading(true);
+    let res = await fetchWeeklyStats(user?.uid as string);
+    setChartLoading(false);
+
+    if (res.success) {
+      setChartData(res?.data?.stats);
+      setTransactions(res?.data?.transactions);
+    } else {
+      Alert.alert("Error", res.msg);
+    }
+  };
+
+  const getMonthlyStats = async () => {
+    setChartLoading(true);
+    let res = await fetchMonthlyStats(user?.uid as string);
+    setChartLoading(false);
+
+    if (res.success) {
+      setChartData(res?.data?.stats);
+      setTransactions(res?.data?.transactions);
+    } else {
+      Alert.alert("Error", res.msg);
+    }
+  };
+
+  const getYearlyStats = async () => {
+    setChartLoading(true);
+    let res = await fetchYearlyStats(user?.uid as string);
+    setChartLoading(false);
+
+    if (res.success) {
+      setChartData(res?.data?.stats);
+      setTransactions(res?.data?.transactions);
+    } else {
+      Alert.alert("Error", res.msg);
+    }
+  };
+
+  useEffect(() => {
+    if (activeIndex == 0) {
+      getWeeklyStats();
+    }
+    if (activeIndex == 1) {
+      getMonthlyStats();
+    }
+    if (activeIndex == 2) {
+      getYearlyStats();
+    }
+  }, [activeIndex]);
+
   return (
     <ScreenWrapper>
-      <Typo>StatisticsScreen</Typo>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Header title="Statistics" />
+        </View>
+        <ScrollView
+          contentContainerStyle={{
+            gap: spacingY._20,
+            paddingTop: spacingY._5,
+            paddingBottom: verticalScale(100),
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <SegmentedControl
+            values={["Weekly", "Monthly", "Yearly"]}
+            tintColor={colors.neutral200}
+            backgroundColor={colors.neutral800}
+            appearance="dark"
+            activeFontStyle={styles.segmentFontStyle}
+            style={styles.segmentStyle}
+            fontStyle={{ ...styles.segmentFontStyle, color: colors.white }}
+            selectedIndex={activeIndex}
+            onChange={(event) =>
+              setActiveIndex(event.nativeEvent.selectedSegmentIndex)
+            }
+          />
+          <View style={styles.chartContainer}>
+            {chartData.length > 0 ? (
+              <BarChart
+                data={chartData}
+                barWidth={scale(12)}
+                spacing={[1, 2].includes(activeIndex) ? scale(25) : scale(16)}
+                minHeight={5}
+                noOfSections={3}
+                isAnimated={true}
+                yAxisLabelPrefix="Rs "
+                yAxisThickness={0}
+                xAxisThickness={0}
+                yAxisLabelWidth={
+                  [1, 2].includes(activeIndex) ? scale(88) : scale(85) // 38 and 35 for $
+                }
+                yAxisTextStyle={{ color: colors.neutral350 }}
+                xAxisLabelTextStyle={{
+                  color: colors.neutral350,
+                  fontSize: verticalScale(12),
+                }}
+                roundedTop
+                roundedBottom
+                hideRules
+              />
+            ) : (
+              <View style={styles.noChart} />
+            )}
+            {chartLoading && (
+              <View style={styles.chartLoadingContainer}>
+                <Loading color={colors.white} />
+              </View>
+            )}
+          </View>
+          <View>
+            <TransactionList
+              title="Transactions"
+              emptyListMessage="No transactions found!"
+              data={transactions}
+            />
+          </View>
+        </ScrollView>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default StatisticsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  chartContainer: {
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  chartLoadingContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    borderRadius: radius._12,
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+  },
+  header: {},
+  noChart: {
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    height: verticalScale(210),
+  },
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    height: verticalScale(35),
+    width: verticalScale(35),
+    borderCurve: "continuous",
+  },
+  segmentStyle: {
+    height: scale(37),
+  },
+  segmentFontStyle: {
+    fontSize: verticalScale(13),
+    fontWeight: "bold",
+    color: colors.black,
+  },
+  container: {
+    paddingHorizontal: spacingX._20,
+    paddingVertical: spacingY._5,
+    gap: spacingY._10,
+  },
+});
--- services/transactionService.ts
+++ services/transactionService.ts
@@ -3,13 +3,21 @@ import {
   deleteDoc,
   doc,
   getDoc,
+  getDocs,
+  orderBy,
+  query,
   setDoc,
+  Timestamp,
   updateDoc,
+  where,
 } from "firebase/firestore";
 import { db } from "@/config/firebaseConfig";
 import { ResponseType, TransactionType, WalletType } from "@/types";
 import { uploadFileToCloudinary } from "./imageService";
 import { createOrUpdateWallet } from "./walletService";
+import { getLast12Months, getLast7Days, getYearsRange } from "@/utils/common";
+import { scale } from "@/utils/styling";
+import { colors } from "@/constants/theme";
 
 export const createOrUpdateTransaction = async (
   transactionData: Partial<TransactionType>
@@ -235,6 +243,192 @@ export const deleteTransaction = async (
     return { success: true };
   } catch (error: any) {
     console.error("Error deleting the transaction: ", error);
+    return { success: false, msg: error.message };
+  }
+};
+
+export const fetchWeeklyStats = async (uid: string): Promise<ResponseType> => {
+  try {
+    const today = new Date();
+    const sevenDaysAgo = new Date(today);
+    sevenDaysAgo.setDate(today.getDate() - 7);
+
+    const transactionsQuery = query(
+      collection(db, "transactions"),
+      where("date", ">=", Timestamp.fromDate(sevenDaysAgo)),
+      where("date", "<=", Timestamp.fromDate(today)),
+      orderBy("date", "desc"),
+      where("uid", "==", uid)
+    );
+
+    const querySnapshot = await getDocs(transactionsQuery);
+    const weeklyData = getLast7Days();
+    const transactions: TransactionType[] = [];
+
+    querySnapshot.forEach((doc) => {
+      const transaction = doc.data() as TransactionType;
+      transaction.id = doc.id;
+      transactions.push(transaction);
+
+      const transactionDate = (transaction.date as Timestamp)
+        .toDate()
+        .toISOString()
+        .split("T")[0];
+
+      const dayData = weeklyData.find((day) => day.date == transactionDate);
+
+      if (dayData) {
+        if (transaction.type === "income") {
+          dayData.income += transaction.amount;
+        } else if (transaction.type === "expense") {
+          dayData.expense += transaction.amount;
+        }
+      }
+    });
+
+    const stats = weeklyData.flatMap((day) => [
+      {
+        value: day.income,
+        label: day.day,
+        spacing: scale(4),
+        labelWidth: scale(30),
+        frontColor: colors.primary,
+      },
+      {
+        value: day.expense,
+        frontColor: colors.rose,
+      },
+    ]);
+
+    return { success: true, data: { stats, transactions } };
+  } catch (error: any) {
+    console.error("Error fetching weekly stats: ", error);
+    return { success: false, msg: error.message };
+  }
+};
+
+export const fetchMonthlyStats = async (uid: string): Promise<ResponseType> => {
+  try {
+    const today = new Date();
+    const twelveMonthsAgo = new Date(today);
+    twelveMonthsAgo.setDate(today.getDate() - 12);
+
+    const transactionsQuery = query(
+      collection(db, "transactions"),
+      where("date", ">=", Timestamp.fromDate(twelveMonthsAgo)),
+      where("date", "<=", Timestamp.fromDate(today)),
+      orderBy("date", "desc"),
+      where("uid", "==", uid)
+    );
+
+    const querySnapshot = await getDocs(transactionsQuery);
+    const monthlyData = getLast12Months();
+    const transactions: TransactionType[] = [];
+
+    querySnapshot.forEach((doc) => {
+      const transaction = doc.data() as TransactionType;
+      transaction.id = doc.id;
+      transactions.push(transaction);
+
+      const transactionDate = (transaction.date as Timestamp).toDate();
+      const monthName = transactionDate.toLocaleString("default", {
+        month: "short",
+      });
+      const shortYear = transactionDate.getFullYear().toString().slice(-2);
+      const monthData = monthlyData.find(
+        (month) => month.month === `${monthName} ${shortYear}`
+      );
+
+      if (monthData) {
+        if (transaction.type === "income") {
+          monthData.income += transaction.amount;
+        } else if (transaction.type === "expense") {
+          monthData.expense += transaction.amount;
+        }
+      }
+    });
+
+    const stats = monthlyData.flatMap((month) => [
+      {
+        value: month.income,
+        label: month.month,
+        spacing: scale(9), // scale(4)
+        labelWidth: scale(30),
+        frontColor: colors.primary,
+      },
+      {
+        value: month.expense,
+        frontColor: colors.rose,
+      },
+    ]);
+
+    return { success: true, data: { stats, transactions } };
+  } catch (error: any) {
+    console.error("Error fetching monthly stats: ", error);
+    return { success: false, msg: error.message };
+  }
+};
+
+export const fetchYearlyStats = async (uid: string): Promise<ResponseType> => {
+  try {
+    const transactionsQuery = query(
+      collection(db, "transactions"),
+      orderBy("date", "desc"),
+      where("uid", "==", uid)
+    );
+
+    const querySnapshot = await getDocs(transactionsQuery);
+    const transactions: TransactionType[] = [];
+
+    const firstTransaction = querySnapshot.docs.reduce((earliest, doc) => {
+      const transactionDate = doc.data().date.toDate();
+      return transactionDate < earliest ? transactionDate : earliest;
+    }, new Date());
+
+    const firstYear = firstTransaction.getFullYear();
+    const currentYear = new Date().getFullYear();
+
+    const yearlyData = getYearsRange(firstYear, currentYear);
+
+    querySnapshot.forEach((doc) => {
+      const transaction = doc.data() as TransactionType;
+      transaction.id = doc.id;
+      transactions.push(transaction);
+
+      const transactionYear = (transaction.date as Timestamp)
+        .toDate()
+        .getFullYear();
+
+      const yearData = yearlyData.find(
+        (item: any) => item.year === transactionYear.toString()
+      );
+
+      if (yearData) {
+        if (transaction.type === "income") {
+          yearData.income += transaction.amount;
+        } else if (transaction.type === "expense") {
+          yearData.expense += transaction.amount;
+        }
+      }
+    });
+
+    const stats = yearlyData.flatMap((year: any) => [
+      {
+        value: year.income,
+        label: year.year,
+        spacing: scale(4), // scale(4)
+        labelWidth: scale(35),
+        frontColor: colors.primary,
+      },
+      {
+        value: year.expense,
+        frontColor: colors.rose,
+      },
+    ]);
+
+    return { success: true, data: { stats, transactions } };
+  } catch (error: any) {
+    console.error("Error fetching yearly stats: ", error);
     return { success: false, msg: error.message };
   }
 };
