8ad27c4a26063923eb2f5ea80131c8cdae434ace
mraza176 <mrazaraheel12@gmail.com>
2025-04-07 22:48:42 +0500

    some improvements

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,20 +1,24 @@
+import { colors } from "@/constants/theme";
 import { JsStack } from "@/layouts/js-stack";
+import { View } from "react-native";
 
 const AuthLayout = () => {
   return (
-    <JsStack
-      screenOptions={{
-        headerShown: false,
-        animation: "slide_from_right",
-        gestureEnabled: true,
-        gestureDirection: "horizontal",
-        gestureResponseDistance: 300,
-      }}
-    >
-      <JsStack.Screen name="welcome" />
-      <JsStack.Screen name="login" />
-      <JsStack.Screen name="register" />
-    </JsStack>
+    <View style={{ flex: 1, backgroundColor: colors.neutral900 }}>
+      <JsStack
+        screenOptions={{
+          headerShown: false,
+          animation: "slide_from_right",
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+          gestureResponseDistance: 300,
+        }}
+      >
+        <JsStack.Screen name="welcome" />
+        <JsStack.Screen name="login" />
+        <JsStack.Screen name="register" />
+      </JsStack>
+    </View>
   );
 };
 
